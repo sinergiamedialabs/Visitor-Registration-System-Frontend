f430cee67ae012af25bbb4bdec890fb311575d95
shameer-simelabs <muhammed.shameer@simelabs.com>
2024-08-13 16:29:52 +0530

    fixed rtk query wrapper issue

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApiProvider } from '@reduxjs/toolkit/query/react';
+import { visitorSystem } from './services/invite';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ApiProvider api={visitorSystem}>
     <App />
+    </ApiProvider>
   </React.StrictMode>
 );
 
