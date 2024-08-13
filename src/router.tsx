1679dee62c4928dceeb505ef97f8ce40bffff1c0
Meera <96071470+MeeraSime@users.noreply.github.com>
2024-08-13 17:20:39 +0530

    invite approval UI,barcode generation

--- src/router.tsx
+++ src/router.tsx
@@ -3,10 +3,15 @@ import {
 } from "react-router-dom";
 import "./index.css";
 import Test from './components/test'
+import { InviteApproval } from "./components/inviteApproval";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Test />,
+  },
+  {
+    path: "/invite_approval",
+    element: <InviteApproval />,
   },
 ]);
